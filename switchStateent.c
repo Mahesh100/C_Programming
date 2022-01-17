0e0b8f317838e18d5980e9d276da9ee2acf007e6
Mahesh <maheshkakde100@gmail.com>
2022-01-17 23:59:27 +0530

    Commit

---
+++ switchStateent.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+//Switch case 
+
+int main(){
+
+    int a =10;
+    int b=20;
+
+    printf("Switch Statement in c\n");
+/*
+    switch(condition/expression/ value)
+    {
+        case lable1: statemnts;
+        break;
+
+        case lable2: statemnts;
+        break;
+        .
+        .
+        .
+        .
+        .
+        default: statemnts;
+        break;
+    }
+*/
+
+return 0;
+}
