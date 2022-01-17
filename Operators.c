be41053385bdee1e1d91c0a6720c1ae7c6bcb904
Mahesh <maheshkakde100@gmail.com>
2022-01-17 12:46:17 +0530

    Arithenatic Operators in C

--- Operators.c
+++ Operators.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /*
 Types of Operators
@@ -11,7 +11,25 @@ Assignment Operators
 Misc Operators
 
  */
-int main(){
+int main()
+{
 
+    int a = 6, b = 7, c = 9;
+
+    printf("The sum of a and b is %d\n", a + b);
+
+    printf("The difference of a and b is %d\n", a-b);
+
+    printf("The multiplication of a and b is %d\n", a*b);
+
+    printf("The division of a and b is %d\n", a/b);
+
+    printf("The increment result is %d\n",a++);
+
+    printf("The decremnet value is %d\n",a--);
+
+    printf("The modulo result is %d\n", a%b);
+
+    
     return 0;
 }
