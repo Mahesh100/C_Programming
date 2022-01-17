6ff558b46e669f1b1acf70b9847f012dcb01dcc9
Mahesh <maheshkakde100@gmail.com>
2022-01-17 11:59:30 +0530

    Opeartors In C

---
+++ Operators.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/*
+Types of Operators
+
+Arithematic Operators
+Relational Operators
+Logical Operators
+Bitwise Operators
+Assignment Operators
+Misc Operators
+
+ */
+int main(){
+
+    return 0;
+}
