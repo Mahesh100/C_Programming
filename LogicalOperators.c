5cec870264b8cde855d2a522f2087454e7368fbc
Mahesh <maheshkakde100@gmail.com>
2022-01-17 14:33:05 +0530

    Logical Opeartor in C

--- LogicalOperators.c
+++ LogicalOperators.c
@@ -6,6 +6,16 @@ int main(){
 
     printf("Logical operators in C");
 
+    int h =0, j=1;
+
+    printf("The logical operator returned %d\n", h&&j);
+
+    printf("The logical operator returned %d\n", h||j);
+
+    printf("The logical operator returned %d\n",!j);
+
+    printf("The logical operator returned%d\n", !h);
+
 
     return 0;
 }
