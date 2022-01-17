4addb97948291160ba8235809227fdc47ab66014
Mahesh <maheshkakde100@gmail.com>
2022-01-17 16:43:02 +0530

    if else stamements in C

--- ifelseInC.c
+++ ifelseInC.c
@@ -11,12 +11,23 @@ int age;
      scanf("%d", &age);
 
     
-
+/*
     if(age<18){
        printf("You not allowed to  drive a car as per the rule by RTO \n");
     }
     else {
         printf("Congurlation ! You are allowed to drive the car");
+    }  */
+
+    if(age<18){
+        printf("You can drive");
     }
+
+    else if(age>=18 && age<=24){
+        printf("You are restricted to drive ");
+    }else{
+        printf("Its temperorly");
+    }
+
     return 0;
 }
