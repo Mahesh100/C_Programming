d71c4e9f3f59162b233dc888da717476775792ee
Mahesh <maheshkakde100@gmail.com>
2022-01-17 13:04:23 +0530

    Logical Opeartors in C

---
+++ LogicalOperators.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    //Logical Operators in C
+
+    printf("Logical operators in C");
+
+
+    return 0;
+}
---
+++ RelationalOpeartors.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    //Arithematic Operators in c
+
+    int a=6, b=7, c=8;
+
+    printf("Arithematic operators in C \n");
+
+    printf("%d\n", a==b);
+
+    printf("%d\n", a!=b);
+
+    printf("%d\n", a>b);
+
+    printf("%d\n", a<b);
+
+
+
+
+    return 0;
+}
+
