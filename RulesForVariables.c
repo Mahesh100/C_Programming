c079738b402cce55cc76b8f4864446773964cffc
Mahesh <maheshkakde100@gmail.com>
2022-01-17 09:24:48 +0530

    Rules for Declearing varible

---
+++ RulesForVariables.c
@@ -0,0 +1,26 @@
+//Rules for creating varibles
+
+#include <stdio.h>
+
+int main(){
+
+    int maheshName;  //camel case notation
+
+    //This is varible declearation
+
+    maheshName =76; //This is variable initalization
+
+ //   int maheshName =58; //This is varible decleartion and initalization
+
+   printf("Roll number of mahesh is %d\n", maheshName);
+
+   //This is constant varaible 
+
+   const  int i = 9;
+
+  //   i=10;
+
+     printf("Value of i is %d\n", i);
+
+     return 0;
+}
