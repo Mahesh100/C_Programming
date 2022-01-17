f146def6347f23a581233a7c4f2bf598589889c7
Mahesh <maheshkakde100@gmail.com>
2022-01-17 15:36:26 +0530

    Type casting in c

---
+++ TypeCasting.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    //Type casting in c 
+   char a= 'm';
+    printf("The sum of two numbers  : %f\n", (float) a);
+
+
+// Division of Two Natural Numbers as follows
+
+//int nun1, num2;
+int num1 =5;
+
+int num2 =2;
+printf("num1/num2 is %f\n ", (float) num1/num2);
+
+
+    return 0;
+}
