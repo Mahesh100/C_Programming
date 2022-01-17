86b1ed3ef1247120d96db847ff49822da7bf04ac
Mahesh <maheshkakde100@gmail.com>
2022-01-17 15:22:19 +0530

    Taking Input from users

---
+++ BitwiseOpeartor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+//Bitwise Opearator
+//Bitwise operators works bit by bit
+
+int a =10; 
+int b=20;
+
+printf("The following are the bitwise operators \n");
+
+printf("Bitwise operator returned %d\n", a&b);
+
+return 0;
+}
---
+++ takingInputFromUsers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int maheshInput;
+
+    printf("Taking Input from users\n");
+
+    printf("Enter the value of maheshInput:-");
+
+    scanf("%d", &maheshInput);
+
+    printf("\n You entered %d as maheshInput", maheshInput);
+}
