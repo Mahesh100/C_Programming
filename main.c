3aa0296b301a1d2b1d82510f7ee10cd7638efe6b
Mahesh <maheshkakde100@gmail.com>
2022-01-17 08:16:38 +0530

    First file Hello World Program

---
+++ main.c
@@ -0,0 +1,15 @@
+/*#include <stdio.h>
+
+int main(){   //int main is Function
+    printf("Hellow World \n Its Mahesh Kakde \n"); //printf is function by stdio.h
+    return 0;  //return value of function
+}  */
+
+#include<stdio.h>
+
+int main(){
+
+    printf("Mahesh Kakde You can do it ");
+    return 0;
+}
+
